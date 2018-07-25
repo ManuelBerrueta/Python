2f0b44eb90bbcc46313042590b0157ddba2fd0fe
manuelberrueta <manuelberrueta@gmail.com>
2018-07-24 21:51:03 -0700

    Added some clarity to the code, and r

--- KeepItFresh/KeepItFresh.py
+++ KeepItFresh/KeepItFresh.py
@@ -4,6 +4,9 @@ import time
 
 NumberOfSites = 19
 CurrentUrl = 0
+SleepTimeinSeconds = 3
+NextUrl = 1
+
 myUrls = ['https://www.stackexchange.com/', 'https://security.stackexchange.com/', 'https://reverseengineering.stackexchange.com/',
 'https://unix.stackexchange.com/', 'https://serverfault.com/', 'https://superuser.com/', 'https://stackoverflow.com/',
 'https://networkengineering.stackexchange.com/', 'https://askubuntu.com/', 'https://scicomp.stackexchange.com/',
@@ -11,13 +14,14 @@ myUrls = ['https://www.stackexchange.com/', 'https://security.stackexchange.com/
 'https://dba.stackexchange.com/', 'https://math.stackexchange.com/', 
 'https://raspberrypi.stackexchange.com/', 'https://iot.stackexchange.com/', 'https://arduino.stackexchange.com/', 
 'https://physics.stackexchange.com/', 'https://sqa.stackexchange.com/']
-WebBrowserDriver = webdriver.Edge('C:\Windows\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\MicrosoftWebDriver.exe')
+
+WebBrowserDriver = webdriver.Edge(r"C:\Windows\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\MicrosoftWebDriver.exe")
 
 while CurrentUrl < NumberOfSites:
     WebBrowserDriver.get(myUrls[CurrentUrl])
-    time.sleep(2)
+    time.sleep(SleepTimeinSeconds)
     WebBrowserDriver.refresh()
-    CurrentUrl += 1
+    CurrentUrl += NextUrl
 
 if CurrentUrl == NumberOfSites:
     WebBrowserDriver.quit()
